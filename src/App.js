09468bf6caeaff03c0660455e1489b6c36edf4d4
williamisfranciscodasilva <williamisepaula@gmail.com>
2018-08-12 18:41:02 -0300

    Alterei o script do arquivo App.js, inserindo assim cabeçalho e corpo no mesmo

--- src/App.js
+++ src/App.js
@@ -3,9 +3,38 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        Olá testando
-      </div>
+      <div>
+          <nav className="navbar navbar-default navbar-fixed-top" role="navigation">
+            <div className="container">
+              <div className="navbar-header page-scroll">
+                <a className="navbar-brand page-scroll" href="#page-top">
+                  <img src="images/logo.png" height="30" />
+                </a>
+              </div>
+
+              <div className="collapse navbar-collapse navbar-ex1-collapse">
+                <ul className="nav navbar-nav">
+                  <li>
+                    <a href="">Menu item</a>
+                  </li>
+                </ul>
+              </div>
+
+            </div>
+          </nav>
+
+
+          <section id="intro" className="intro-section">
+            <div className="container">
+              <div className="row">
+                <div className="col-lg-12">
+                  <h1><img src="images/logo.png" /></h1>
+                  <p>Nunca mais esqueça uma série que você assistiu ou que alguém lhe indicou.</p>
+                </div>
+              </div>
+            </div>
+          </section>
+      </div>  
     )
   }
 }
